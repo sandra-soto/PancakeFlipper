9d12780a2e4428a6ebaddbdaa723a321662bce51
Luis Luna <lfluna@uci.edu>
2019-01-07 09:44:49 -0800

    Patrol doc

--- Platformer/Assets/Scripts/Patrol.cs
+++ Platformer/Assets/Scripts/Patrol.cs
@@ -4,31 +4,39 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour {
 
-    public float moveSpeed;
+    /// <summary>
+    /// Patrol takes a GameObject and makes such object to patrol specified locations at the given speed
+    /// </summary>
+    
+    public float moveSpeed; //Patrol speed
+
     [Header("Agent's patrol areas")]
-    public List<Transform> patrolLocations;
+    public List<Transform> patrolLocations; //List of all the Transform locations the gameObject will patrol
+
     [Space]
     [Header("Agent")]
-    public GameObject patrollingGameObject;
-    private int nextPatrolLocation;
+    public GameObject patrollingGameObject; //Unity GameObject that patrols
+    private int nextPatrolLocation; //Keeps track of the patrol location
 	
 	// Update is called once per frame
 	void Update () {
         PatrolArea();
 	}
 
+    //Moves the patrollingGameObject towards patrol location, when reach destination switch to next patrol position in the list
     private void PatrolArea()
     {
+        Flip();
         patrollingGameObject.transform.position = Vector2.MoveTowards(patrollingGameObject.transform.position,
             patrolLocations[nextPatrolLocation].position, moveSpeed * Time.deltaTime);
 
-        Flip();
         if (Vector2.Distance(patrollingGameObject.transform.position, patrolLocations[nextPatrolLocation].position) <= 2)
         {
-            nextPatrolLocation = (nextPatrolLocation + 1) % patrolLocations.Count;
+            nextPatrolLocation = (nextPatrolLocation + 1) % patrolLocations.Count; //Prevents IndexOutofBound by looping back through list
         }
     }
 
+    //Makes the patrollingGameObject always be facing the next patrol location
     private void Flip()
     {
         Vector2 localScale = patrollingGameObject.transform.localScale;
